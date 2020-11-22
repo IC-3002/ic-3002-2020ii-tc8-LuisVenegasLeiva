7c93b3959f3d81652472d902d68f04ba5aaba522
LuisVenegas <@luvenegas>
2020-11-22 11:33:45 -0600

    Se realizan algunas modificaciones minimas

--- rutas.py
+++ rutas.py
@@ -1,40 +1,34 @@
 def verificar_ruta(x, y,M,N):
     return not (x < 0 or y < 0 or x >= M or y >= N)
  
- 
 def contar_rutas_mas_cortas(C):
     N = len(C)
     M = len(C[1])
 
-    count = 0
+    contador = 0
  
     revisadas = [[False for x in range(N)] for y in range(M)]
  
+    contador = buscar(C, 0, 0, revisadas, contador,M,N)
+    return contador
 
-    count = buscar(C, 0, 0, revisadas, count,M,N)
-    return count
-
-
-def buscar(C, x, y, revisadas, count, M, N):
+def buscar(C, x, y, revisadas, contador, M, N):
     if x == M - 1 and y == N - 1 and C[N-1][M-1]==1:
-        return count
+        return contador
     if x == M - 1 and y == N - 1:
-        return count + 1
- 
+        return contador + 1
 
     revisadas[x][y] = True
- 
 
     if verificar_ruta(x, y, M, N) and C[x][y] == 0:
- 
         if x + 1 < M and not revisadas[x + 1][y]:
-            count = buscar(C, x + 1, y, revisadas, count, M, N)
+            contador = buscar(C, x + 1, y, revisadas, contador, M, N)
  
         if y + 1 < N and not revisadas[x][y + 1]:
-            count = buscar(C, x, y + 1, revisadas, count, M, N)
+            contador = buscar(C, x, y + 1, revisadas, contador, M, N)
 
     revisadas[x][y] = False
-    return count
+    return contador
 '''
 C =[[0, 0, 0, 0],
     [0, 0, 0, 0],
